3131dca9c0a009470047b8eca7e4557dfc0d5ee3
Cameron Day <camerond594@gmail.com>
2024-08-25 20:10:17 -0700

    Sum stats by game_type
    The default game_type is always regular_season.

--- app/models/player.rb
+++ app/models/player.rb
@@ -32,13 +32,13 @@ class Player < ApplicationRecord
     %w[full_name total_games_played total_goals total_assists total_points]
   end
 
-  def self.summed_stat(stat_name:)
+  def self.summed_stat(stat_name:, game_type: "regular_season")
     Arel.sql(
       "COALESCE((
         SELECT SUM(player_stats.#{stat_name})
         FROM player_stats
         JOIN roster_assignments ON roster_assignments.id = player_stats.roster_assignment_id
-        WHERE roster_assignments.player_id = players.id
+        WHERE roster_assignments.player_id = players.id AND game_type = #{PlayerStat.game_types[game_type]}
         GROUP BY roster_assignments.player_id
       ), 0)"
     )
