dce35ab53a3fcac8e9321ac0425fc26d951a9800
Cameron Day <camerond594@gmail.com>
2024-08-26 19:36:21 -0700

    Show a player's age
    Given their birth date, we should be able to show how old they are.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+require 'date'
+
 module ApplicationHelper
+  def years_between(earlier_date:, later_date:)
+    # Calculate the difference in years.
+    year_diff = later_date.year - earlier_date.year
+    
+    # Adjust if the later date is before the anniversary of the earlier date in the same year.
+    year_diff -= 1 if later_date < earlier_date.next_year(year_diff)
+    
+    year_diff
+  end
 end
